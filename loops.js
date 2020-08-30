79ecf03686e7c92de887d9562ac8cc3e764df1bd
Sarah Kain <sarahvkain@gmail.com>
2020-08-30 17:45:46 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array) {
+  for(let i = 0; i < 25; i++){
+    if(i === 1){
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+function whileLoop(num) {
+  let i = num
+  while(i > 0){
+    console.log(num);
+    i--;
+  }
+  return 'done'
+}
